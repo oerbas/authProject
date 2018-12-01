728ddf753ea9faba859dc6324331b80fef9718bd
Orhan Erbas <orhanerba@icloud.com>
2018-12-01 20:35:10 +0300

    Adding inputs and login form and some styling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+Assuming that you have Node installed, you can use npm to install the Expo CLI command line utility:
+    
+    -$npm install -g expo-cli
+
 Start Project:
-  - $expo start
+    
+    -$expo start
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common/Header';
+import LoginForm from './components/LoginForm';
 
 class Index extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyA3fx8_TEOmvg-TOah_jBVlBLzZ3FcDTiA",
+            authDomain: "auth-b025e.firebaseapp.com",
+            databaseURL: "https://auth-b025e.firebaseio.com",
+            projectId: "auth-b025e",
+            storageBucket: "auth-b025e.appspot.com",
+            messagingSenderId: "880837806161"
+        });
+    }
+
     render() {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <Text>Open up App.js to start working on your app!</Text>
+                <LoginForm />
             </View>
         )
     }
