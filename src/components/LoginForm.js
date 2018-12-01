1574f32867003de6f074a1b78a526cdf487766cf
Orhan Erbas <orhanerba@icloud.com>
2018-12-01 20:50:03 +0300

    firebase auth

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from "./common";
 
 class LoginForm extends Component{
     state = { email: '', password: '' };
+
+    onButtonPress(){
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 
     render() {
         return (
@@ -24,9 +30,8 @@ class LoginForm extends Component{
                       onChangeText={password => this.setState({ password })}
                   />
               </CardSection>
-
               <CardSection>
-                  <Button>
+                  <Button onPress={ this.onButtonPress.bind(this) }>
                       Log in
                   </Button>
               </CardSection>
